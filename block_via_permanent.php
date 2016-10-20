fbf6c5c2b0f8cd560ad15c36350659fa474c36f8
SVIeSolutions <alexandra.dinan@sviesolutions.com>
2016-10-20 08:15:09 -0400

    Version 2016200405
    Corrections following moodle.org's comments.
    Modification to plugin name and we validate if the user is associated
    before displaying the link.

--- block_via_permanent.php
+++ block_via_permanent.php
@@ -58,11 +58,12 @@ class block_via_permanent extends block_list {
         $this->content->icons = array();
 
         $sql = "SELECT cm.id, v.name  FROM mdl_via v
-            JOIN mdl_course_modules cm ON v.id = cm.instance
+            JOIN mdl_course_modules cm ON v.id = cm.instance AND cm.course = v.course
             JOIN mdl_modules m ON m.id = cm.module
-            WHERE v.activitytype = ? AND m.name = ? AND v.course = ? AND cm.course = ? AND cm.visible = ?
+			JOIN moodletest.mdl_via_participants vp ON vp.activityid = v.id
+            WHERE v.activitytype = 2 AND m.name = 'via' AND v.course = ? AND cm.visible = 1 AND vp.userid = ?
             ORDER BY v.name DESC";
-        $result = $DB->get_records_sql($sql, array(2, 'via', $courseid, $courseid, 1));
+		$result = $DB->get_records_sql($sql, array($courseid, $USER->id));
 
         if ($result) {
             foreach ($result as $row) {
--- lang/en/block_via_permanent.php
+++ lang/en/block_via_permanent.php
@@ -31,4 +31,3 @@ $string['configadminview'] = 'What should the admin see in the course list block
 $string['confighideallcourseslink'] = 'Hide "All courses" link at the bottom of the block. Link hiding does not affects Admin\'s view';
 $string['hideallcourseslink'] = 'Hide All courses link';
 $string['owncourses'] = 'Admin user sees own courses';
-$string['pluginname'] = 'Courses';
